821c92421e6407ece1355bfbd317a283197c69eb
Austin Jenchi <ajenchi9@gmail.com>
2023-11-27 00:51:58 -0800

    Add an autocomplete search to alliance selection teams

--- src/components/AllianceSelectionCard.tsx
+++ src/components/AllianceSelectionCard.tsx
@@ -1,3 +1,4 @@
+import Autosuggest from '@cloudscape-design/components/autosuggest';
 import Box from '@cloudscape-design/components/box';
 import Button from '@cloudscape-design/components/button';
 import Container from '@cloudscape-design/components/container';
@@ -5,6 +6,7 @@ import Header from '@cloudscape-design/components/header'
 import SpaceBetween from '@cloudscape-design/components/space-between';
 import Spinner from '@cloudscape-design/components/spinner';
 import TeamInfoCache from '../TeamInfoCache';
+import { useState } from 'react';
 
 const loadingPlaceholder = <Container><Box textAlign='center'><Spinner size='large' /></Box></Container>;
 
@@ -13,13 +15,33 @@ interface AllianceSelectionCardProps {
     setModalTeam: (val: number | null) => void,
 }
 
-export default function AllianceSelectionCardProps({teamInfoCache, setModalTeam}: AllianceSelectionCardProps) {
+export default function AllianceSelectionCard({teamInfoCache, setModalTeam}: AllianceSelectionCardProps) {
     if (teamInfoCache.allTeams().length == 0) return loadingPlaceholder;
     return (
         <Container header={<Header variant='h2'>Teams</Header>}>
-            <SpaceBetween direction='horizontal' size='s'>
-                {teamInfoCache.allTeams().map((team, idx) => <Button key={idx} onClick={() => setModalTeam(team)}>{team}</Button>)}
+            <SpaceBetween direction='vertical' size='s'>
+                <TeamAutocomplete teamInfoCache={teamInfoCache} setModalTeam={setModalTeam} />
+                <SpaceBetween direction='horizontal' size='s'>
+                    {teamInfoCache.allTeams().map((team, idx) => <Button key={idx} onClick={() => setModalTeam(team)}>{team}</Button>)}
+                </SpaceBetween>
             </SpaceBetween>
         </Container>
+    );
+}
+
+function TeamAutocomplete({teamInfoCache, setModalTeam}: AllianceSelectionCardProps) {
+    const [value, setValue] = useState('');
+    return (
+        <Autosuggest onChange={({detail}) => setValue(detail.value)}
+            value={value}
+            options={teamInfoCache.allTeams().map(team => ({value: team.toString()}))}
+            placeholder='Team number'
+            onSelect={({detail}) => {
+                setModalTeam(parseInt(detail.value));
+                setValue('');
+            }}
+            ariaLabel='Team number'
+            enteredTextLabel={value => `Use: "${value}"`}
+        />
     );
 }
